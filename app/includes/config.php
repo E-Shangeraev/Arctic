ae88722cce10f327c521ec1d70edc56c0a619d3f
E-Shangeraev <e.shangeraev@gmail.com>
2020-09-24 21:47:13 +0700

    Подключение БД. Работа над блогом.

---
+++ app/includes/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'mysql',
+        'password' => 'mysql',
+        'name' => 'arctic_db'
+    )
+);
+
+require 'db.php';
